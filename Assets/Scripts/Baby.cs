5777325b981df8d7b4a338f961948c46812da429
Daniel Waterston <DanielAfroHead@live.com.au>
2015-03-18 11:32:51 +1100

    Added current baby script and .gitignore that will cut out temp files we don't need

---
+++ Assets/Scripts/Baby.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baby : MonoBehaviour 
+{
+    //How sad the baby is
+    public int Sadness = 0;
+    //How loud the baby is
+    public int Loudness = 0;
+    //Is holding a dangerous item
+    public bool DangerousGoods = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+    void SwipeHazzard()
+    {
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
