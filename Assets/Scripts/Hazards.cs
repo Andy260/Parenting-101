d95d5d7a228f465b6982210df0351028d462722b
Andrew Barbour <andrew.barbour1@gmail.com>
2015-03-23 16:29:13 +1100

    Added sprites for buttons and banner within Menu Scene.

---
+++ Assets/Scripts/Hazards.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazards : MonoBehaviour 
+{
+    public bool _leverActivated;
+    ParticleEmitter _wireParticles;
+
+	void Start () 
+    {
+	    
+	}
+
+	void Update () 
+    {
+	    
+	}
+
+
+    /// <summary>
+    /// Starts leaver hazard
+    /// </summary>
+    public void StartLeaverHazard()
+    {
+
+    }
+
+    void StopLeaverHazard()
+    {
+
+    }
+}
