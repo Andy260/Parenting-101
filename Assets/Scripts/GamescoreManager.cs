890dd35019aa8ab5a9d7917b7c3fa7dea1a6df80
Andrew Barbour <andrew.barbour1@gmail.com>
2015-03-24 18:53:08 +1100

    Added score manager, which displays the player's current score and saves highest score while changing scenes once the Gameover() function is called.

---
+++ Assets/Scripts/GamescoreManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GamescoreManager : MonoBehaviour 
+{
+	public Text _highscoreDisplay;
+	public int _score;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		// Update score display
+		_highscoreDisplay.text = "Score: " + _score.ToString ();
+	}
+
+	public void AddScore(int a_score)
+	{
+		_score += a_score;
+	}
+
+	public void Gameover()
+	{
+		// Save new score if higher than saved score
+		int savedScore = PlayerPrefs.GetInt ("p101_highscore");
+		if (savedScore < _score)
+		{
+			PlayerPrefs.SetInt("p101_highscore", _score);
+		}
+
+		// Change level
+		Application.LoadLevel ("MenuState");
+	}
+}
